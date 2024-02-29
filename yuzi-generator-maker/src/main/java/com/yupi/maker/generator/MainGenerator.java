bdab7c17a26c194817bbc87752c2777003ae62c3
zrj <819519226@qq.com>
2024-02-29 22:28:06 +0800

    5、制作工具优化(模板方式模式)

--- yuzi-generator-maker/src/main/java/com/yupi/maker/generator/MainGenerator.java
+++ yuzi-generator-maker/src/main/java/com/yupi/maker/generator/MainGenerator.java
@@ -1,18 +1,5 @@
 package com.yupi.maker.generator;
 
-import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.io.resource.ClassPathResource;
-import cn.hutool.core.util.StrUtil;
-import com.yupi.maker.generator.JarGenerator;
-import com.yupi.maker.generator.file.DynamicFileGenerator;
-import com.yupi.maker.generator.file.StaticFileGenerator;
-import com.yupi.maker.meta.Meta;
-import com.yupi.maker.meta.MetaManager;
-import freemarker.template.TemplateException;
-
-import java.io.File;
-import java.io.IOException;
-
 public class MainGenerator extends GeneratorTemplate{
     @Override
     protected void buildDist(String outputPath, String sourceCopyDestPath, String jarPath, String shellOutputFilePath) {
